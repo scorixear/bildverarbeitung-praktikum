92caab6da8f6da09a8c481811701f3cd79abdb61
Paul Keller <contact.keller.paul@gmail.com>
2023-04-04 11:23:29 +0200

    formatting

--- Woche_1/solution.py
+++ Woche_1/solution.py
@@ -4,43 +4,43 @@ import numpy as np
 from PIL import Image
 
 def main():
-  # 1. Show Pictures
-  image = Image.open("Woche_1/test.jpg")
-  numpy_data = np.asarray(image, dtype=np.uint8)
-  plt.axis("off")
-  plt.imshow(numpy_data)
-  plt.show()
-  
-  # 2. Tiling
-  tiled_image = tile_image(numpy_data, 3, 4)
-  plt.axis("off")
-  plt.imshow(tiled_image)
-  plt.show()
-  
-  # 3.
-  cropped_image = crop(numpy_data, 100, 100, 200, 200)
-  plt.axis("off")
-  plt.imshow(cropped_image)
-  plt.show()
+    # 1. Show Pictures
+    image = Image.open("Woche_1/test.jpg")
+    numpy_data = np.asarray(image, dtype=np.uint8)
+    plt.axis("off")
+    plt.imshow(numpy_data)
+    plt.show()
+
+    # 2. Tiling
+    tiled_image = tile_image(numpy_data, 3, 4)
+    plt.axis("off")
+    plt.imshow(tiled_image)
+    plt.show()
+
+    # 3.
+    cropped_image = crop(numpy_data, 100, 100, 200, 200)
+    plt.axis("off")
+    plt.imshow(cropped_image)
+    plt.show()
 
 def tile_image(image, rows, cols):
-  if cols == 1:
-    tiled_image = image
-  else:
-    lst_images = []
-    for _ in range(cols):
-      lst_images.append(image)
-    tiled_image = np.concatenate(lst_images, axis=1)
-  if rows > 1:
-    lst_images = []
-    for _ in range(rows):
-      lst_images.append(tiled_image)
-    tiled_image = np.concatenate(lst_images, axis=0)
-  return tiled_image
+    if cols == 1:
+        tiled_image = image
+    else:
+        lst_images = []
+        for _ in range(cols):
+            lst_images.append(image)
+        tiled_image = np.concatenate(lst_images, axis=1)
+    if rows > 1:
+        lst_images = []
+        for _ in range(rows):
+            lst_images.append(tiled_image)
+        tiled_image = np.concatenate(lst_images, axis=0)
+    return tiled_image
 
 def crop(image, x1, y1, x2, y2):
-  cropped_image = image[y1:y2, x1:x2]
-  return cropped_image
+    cropped_image = image[y1:y2, x1:x2]
+    return cropped_image
 
 if __name__ == "__main__":
-  main()
+    main()
